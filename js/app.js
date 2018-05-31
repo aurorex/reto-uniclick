73ea175469828f4bd166c97a8f6af3571a61d037
aurorex <selva_beat@hotmail.com>
2018-05-31 10:57:08 -0500

    subiendo cambios y funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
 (function() {
+  const inputMonto = document.getElementById('monto');
+  const inputAños = document.getElementById('años');
+  const btnSimular = document.getElementById('simular');
+  const tbody = document.getElementById('tbody');
+  
+  inputMonto.addEventListener('input', function(){
+    inputYear.addEventListener('input',function(){
+      var url = `http://lectorweb.uniclickdigital.com/api/ahorro/${inputMont.value}/${inputYear.value}`;
+      btnSimular.addEventListener('click', function() {
+        fetch(url)
+            .then((response) => {
+              return response.json();
+            })
+            .then((recurso)=> {
+              console.log(recurso);
+            //   for(let i=0;i<){
+            //     tbody.innerHTML = ` <tr>
+            //   <td>${recurso.}</td>
+            //   <td>${}</td>
+            //   <td>${}</td>
+            // </tr>`
+
+            //   }
+
+              
+    
+        });
+
+    })
+   
+
+
+
+  })
+
+  
+
+
+  });
 
 })();
